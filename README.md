a84db71faf3cad9b2af8f504bc24482c5e8902f6
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-08-07 21:35:49 +0200

    commit

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 live on Codepen:
 
++ <a href="https://codepen.io/TomaszPieta/pen/ejrGEN">vue-counter</a>
 + <a href="https://codepen.io/TomaszPieta/pen/mKKazv">vue-list-filtering</a>
---
+++ vue-counter/app.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <button v-on:click="count++" class="plus">Plus</button>
+          <span class="counter">{{count}}</span>
+        <button v-on:click="count--" class="minus">Minus</button>
+    </div>
+</template>
+
+<script>
+new Vue({
+    el: '.container',
+    data: { count: 0 }
+    })
+// export default {
+    
+// }
+</script>
+
+<style lang="scss">
+body {
+  background-color: #0059ff;
+  margin: 40vh 25vw;
+}
+body, button {
+  font-size: 2rem;
+  font-family: sans-serif;
+  text-align: center;
+}
+.container {
+  width: 22rem;
+}
+%space {
+  background-color: yellow;
+  border: 2px solid #000;
+  padding: 0rem 1rem;
+  outline: none;
+  height: 4rem;
+}
+.counter {
+  @extend %space;
+  border-radius: 100%;
+  display: inline-block;
+  line-height: 4rem;  
+  width: 4rem;
+}
+button {
+  @extend %space;
+  border-radius: .5rem;
+}
+</style>
